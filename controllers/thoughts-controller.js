81fa09503f9ea89f9799a60a2adeebffc635c144
Jeff <jcgilbert70@gmail.com>
2023-02-13 09:58:29 -0600

    pseudo coded controllers

---
+++ controllers/thoughts-controller.js
@@ -0,0 +1,22 @@
+const {Thoughts, Users} = require('../models');
+
+const thoughtsController = {
+/*
+    createThoughts
+
+    getAllThoughts
+
+    getThoughtsById
+
+    updateThoughts
+
+    deleteThoughts
+
+    addReaction
+
+    deleteReaction
+*/
+
+};
+
+module.exports = thoughtsController;
---
+++ controllers/users-controller.js
@@ -0,0 +1,22 @@
+const { Users } = require('../models');
+
+const usersController = {
+/*
+    createUsers
+
+    getAllUsers
+
+    getUsersById
+
+    updateUsers
+
+    deleteUsers
+
+    addFriend
+
+    deleteFriend
+*/
+
+};
+
+module.exports = usersController;
